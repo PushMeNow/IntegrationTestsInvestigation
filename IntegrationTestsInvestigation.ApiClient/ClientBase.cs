56a02825f8e5b0403de85565f982f0c1956789cc
Konstantin Borovik <kes8594@yandex.ru>
2022-06-15 01:48:12 +0300

    - init

---
+++ IntegrationTestsInvestigation.ApiClient/ClientBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IntegrationTestsInvestigation.ApiClient
+{
+    public abstract class ClientBase : IDisposable
+    {
+        private readonly HttpClient _client;
+        
+        protected ClientBase()
+        {
+            _client = new HttpClient();
+        }
+
+        public Task<HttpRequestMessage> CreateHttpRequestMessageAsync(CancellationToken token)
+        {
+            return Task.FromResult(new HttpRequestMessage());
+        }
+
+        public Task<HttpClient> CreateHttpClientAsync(CancellationToken token)
+        {
+            return Task.FromResult(_client);
+        }
+
+        public void Dispose()
+        {
+            _client?.Dispose();
+        }
+    }
+}
---
+++ IntegrationTestsInvestigation.Tests/FixtureBase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Net.Http;
+using IntegrationTestsInvestigation.ApiClient;
+
+namespace IntegrationTestsInvestigation.Tests
+{
+    public class FixtureBase<TApiClient> : IDisposable
+        where TApiClient : ClientBase
+    {
+        private static readonly Type ApiType = typeof(TApiClient);
+        private static readonly Type HttpType = typeof(HttpClient);
+        private static readonly Func<TApiClient> Creator = GetCreator();
+
+        private TApiClient _client;
+
+        public TApiClient Client => _client ?? Creator.Invoke();
+
+        private static Func<TApiClient> GetCreator()
+        {
+            var http = Expression.Constant(new HttpClient());
+            var newExpression = Expression.New(ApiType.GetConstructor(new[]
+                                                                      {
+                                                                          HttpType
+                                                                      }),
+                                               http);
+
+            return Expression.Lambda<Func<TApiClient>>(newExpression)
+                             .Compile();
+        }
+
+        public void Dispose()
+        {
+            _client?.Dispose();
+        }
+    }
+}
